2e8be9997f0971528e8b4a08125dd33b22295374
mitnk <whgking@gmail.com>
2012-02-25 13:49:57 +0800

    first commit.

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'kindleio.views.home', name='home'),
+    # url(r'^kindleio/', include('kindleio.foo.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'kindleio.views.home', name='home'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
