b57f880f2efd6ddd3c0e49f6cf69c94131b58863
mitnk <whgking@gmail.com>
2012-02-27 15:36:38 +0800

    Added home page(html).

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', 'kindleio.views.home', name='home'),
+    url(r'^login/$', 'kindleio.views.login', name='login'),
 )
 
 if settings.DEBUG:
--- views.py
+++ views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
+    return render_to_response('home.html',
+                              context_instance=RequestContext(request))
+
+def login(request):
     return render_to_response('login.html',
                               context_instance=RequestContext(request))
 
